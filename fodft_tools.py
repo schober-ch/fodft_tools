90a447c0b297e4dd28e3d3079b9cf34314dfe6f0
Christoph Schober <christoph.schober@ch.tum.de>
2013-11-18 11:23:19 +0100

    Working. Hacking-style code...

--- fodft_tools.py
+++ fodft_tools.py
@@ -7,7 +7,7 @@ from ase.calculators.aims import Aims
 
 # general stuff
 
-species_path = "/data/schober/code/fhiaims_develop/fhiaims_supporting_work/species_defaults/tight"
+#species_path = "/data/schober/code/fhiaims_develop/fhiaims_supporting_work/species_defaults/tight"
 
 def create_fragments(atoms, fo_files=None):
     """ Function to create fragments for FO-DFT from complete cell
@@ -35,12 +35,80 @@ def create_fragments(atoms, fo_files=None):
 
     return frag1, frag2
 
+def write_fo(atoms, step):
+    # define basic parameters
+    if step is "fo":
+        str_fo_dft = "final"
+    else:
+        str_fo_dft = "fragment"
+    print("write_fo: {0}, {1}".format(step, str_fo_dft))
+    calc = Aims(xc="blyp",
+                spin="collinear",
+                occupation_type="gaussian 0.01",
+                mixer="pulay",
+                n_max_pulay="10",
+                charge_mix_param="0.5",
+                sc_accuracy_rho=1E-4,
+                sc_accuracy_eev=1E-2,
+                sc_accuracy_etot=1E-6,
+                sc_iter_limit=100,
+                fo_dft=str_fo_dft,
+                #fo_options=step,
+                #fo_embedding=".false.",
+                relativistic="none",
+                species_dir="/data/schober/code/fhiaims_develop/fhiaims_supporting_work/species_defaults/" + basis_type)
 
+    atoms.set_calculator(calc)
+
+    #fo_emb = raw_input("Use fo_embedding? [y/n, default: n] ")
+    #if fo_emb == "":
+        #fo_emb = "n"
+
+    if str_fo_dft is "fragment":
+        atoms.calc.set(fo_options=step)
+        if fo_emb is "y":
+            atoms.calc.set(fo_embedding=".true.")
+
+    #if fo_emb is "y":
+        #atoms.calc.set(fo_embedding=".true.")
+    if step is "fo":
+        atoms.calc.set(fo_orbitals="x x 1 1 hole")
+
+    atoms.calc.set(charge=charges[step])
+    #atoms.calc.set(default_initial_moment=abs(charges[step])
+    
+
+    atoms.calc.write_control(atoms, os.path.join(step, "control.in"))
+    atoms.calc.write_species(atoms, os.path.join(step, "control.in"))
+                
+
+############################################################################
+###################################### CODE ################################
+############################################################################
 
 dimer_file = sys.argv[1]
 
+try:
+    basis_type = sys.argv[2]
+except:
+    basis_type = "tight"
+
 print("############################################")
 geom_format = raw_input("Format of coord-file (default: xyz): ")
+
+fo_emb = raw_input("Use fo_embedding? [y/n, default: n] ")
+if fo_emb == "":
+    fo_emb = "n"
+
+charges = {"frag1": 0, "frag2": 0, "fo":0}
+t_charges = raw_input("Charges on [frag1], [frag2] (Input-example: +1 0): ")
+
+charges["frag1"] = float(t_charges.strip().split()[0])
+charges["frag2"] = float(t_charges.strip().split()[1])
+charges["fo"] = charges["frag1"] + charges["frag2"]
+
+if geom_format is "":
+    geom_format = "xyz"
 
 dimer = read(dimer_file, format=geom_format)
 
@@ -65,7 +133,12 @@ concat_dimer = frag1 + frag2
 
 write("fo/geometry.in", concat_dimer, format="aims")
 
+# create dictionary of atom objects
+atom_obj = {"frag1": frag1,
+            "frag2": frag2,
+            "fo": concat_dimer}
+
 for step in folders:
-    write_fo(step)
+    write_fo(atom_obj[step], step)
 
 
